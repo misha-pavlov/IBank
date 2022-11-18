a838d11cf08736f80403547d36475c793e59ec4b
misha-pavlov <m.pavlov@apiko.com>
2022-11-18 16:35:07 -0600

    feat(card operations): add 3d flip for card

--- mobile/IBank/components/Card/Card.tsx
+++ mobile/IBank/components/Card/Card.tsx
@@ -1,7 +1,9 @@
-import React, { FC, useEffect, useState } from 'react';
+import React, { FC, memo, RefObject, useEffect, useRef, useState } from 'react';
 import moment from 'moment';
 import { isFunction } from 'lodash';
 import { View } from 'native-base';
+import CardFlip from 'react-native-card-flip';
+import FlipCard from 'react-native-card-flip';
 import { BankName, BottomBlock, CardBlock, CardNumber, cardStyles, ExpiredDate } from './Card.styles';
 import { constants } from '../../config/constants';
 import { TCard } from './Card.types';
@@ -14,6 +16,8 @@ const Card: FC<TCard> = ({ cardNumber, withFlip, expiredDate, withFullWidth, isM
   const [showBanner, setShowBanner] = useState(false);
   const isBlackCard = type === CardType.BLACK;
 
+  const cardFlipRef = useRef() as RefObject<FlipCard> | undefined;
+
   useEffect(() => {
     let timeout: NodeJS.Timeout;
 
@@ -24,48 +28,65 @@ const Card: FC<TCard> = ({ cardNumber, withFlip, expiredDate, withFullWidth, isM
     return () => timeout && clearInterval(timeout);
   }, [showBanner]);
 
+  const stylesCardFlip = { height: withFullWidth ? 220 : 185 };
+
   return (
-    <CardBlock
-      type={type}
-      disabled={!onLongPress}
-      onLongPress={() => {
-        if (isFunction(onLongPress)) {
-          onLongPress();
-        }
+    // use ts-ignore here because CardFlip get undefined error
+    // @ts-ignore
+    <CardFlip style={stylesCardFlip} ref={cardFlipRef}>
+      <CardBlock
+        type={type}
+        withFullWidth={withFullWidth}
+        onLongPress={() => {
+          if (isFunction(onLongPress)) {
+            onLongPress();
+          }
 
-        setShowBanner(true);
-      }}
-      withFullWidth={withFullWidth}
-      style={withFlip ? cardStyles.flip : cardStyles.empty}>
-      <BankName useWhiteColor={isBlackCard}>{constants.appName}</BankName>
-      <CardNumber withFullWidth={withFullWidth} isBlackCard={isBlackCard}>
-        {cardNumber.replace(/(.{4})/g, '$1 ')}
-      </CardNumber>
+          setShowBanner(true);
+        }}
+        disabled={!onLongPress || !withFullWidth}
+        style={withFlip ? cardStyles.flip : cardStyles.empty}
+        onPress={() => cardFlipRef?.current && cardFlipRef?.current.flip()}>
+        <BankName useWhiteColor={isBlackCard}>{constants.appName}</BankName>
+        <CardNumber withFullWidth={withFullWidth} isBlackCard={isBlackCard}>
+          {cardNumber.replace(/(.{4})/g, '$1 ')}
+        </CardNumber>
 
-      <BottomBlock>
-        <ExpiredDate isBlackCard={isBlackCard}>{moment(expiredDate).format('MM/YY')}</ExpiredDate>
-        {isMasterCard ? <MasterCard width={50} height={50} /> : <Visa width={50} height={50} />}
-      </BottomBlock>
+        <BottomBlock>
+          <ExpiredDate isBlackCard={isBlackCard}>{moment(expiredDate).format('MM/YY')}</ExpiredDate>
+          {isMasterCard ? <MasterCard width={50} height={50} /> : <Visa width={50} height={50} />}
+        </BottomBlock>
 
-      {showBanner && (
-        <View
-          top={0}
-          left={0}
-          right={0}
-          height="30px"
-          position="absolute"
-          justifyContent="center"
-          borderTopLeftRadius={20}
-          borderTopRightRadius={20}
-          backgroundColor={colors.green1}
-          w={withFullWidth ? '111%' : '300px'}>
-          <WhiteText textAlign="center" fontWeight={500}>
-            Coppied!
-          </WhiteText>
+        {showBanner && (
+          <View
+            top={0}
+            left={0}
+            right={0}
+            height="30px"
+            position="absolute"
+            justifyContent="center"
+            borderTopLeftRadius={20}
+            borderTopRightRadius={20}
+            backgroundColor={colors.green1}
+            w={withFullWidth ? '111%' : '300px'}>
+            <WhiteText textAlign="center" fontWeight={500}>
+              Coppied!
+            </WhiteText>
+          </View>
+        )}
+      </CardBlock>
+
+      <CardBlock
+        type={type}
+        withFullWidth={withFullWidth}
+        style={withFlip ? cardStyles.flip : cardStyles.empty}
+        onPress={() => cardFlipRef?.current && cardFlipRef?.current.flip()}>
+        <View w="100%" height="100%" alignItems="flex-end" justifyContent="center">
+          <WhiteText fontSize={18}>123</WhiteText>
         </View>
-      )}
-    </CardBlock>
+      </CardBlock>
+    </CardFlip>
   );
 };
 
-export default Card;
+export default memo(Card);
